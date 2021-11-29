cd5c78edc2f2140442c72b12f4247aa3fd387cd9
Harsh Singh <kmrharsh50@gmail.com>
2021-11-29 08:39:01 +0530

    [Harsh]Added:Validating First Name Regex_Problem feature implemented

---
+++ Regex Problem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Regex_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Regex Program Press-\n 1.Validate First Name\n 2.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("User Registration Validating using Regular Expression!");
+                        Sample regexSample = new Sample();
+                        Console.WriteLine("Enter the First Name to Validate:");
+                        string input = Console.ReadLine();
+                        regexSample.ValidatingFirstName(input);
+                        regexSample.Validating();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Regex Problem/Sample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex_Problem
+{
+    class Sample
+    { 
+            string pattern = "^[A-Za-z]{2,}$";
+            public void ValidatingFirstName(string input)
+            {
+                Regex regex = new Regex(pattern);
+
+                bool res = regex.IsMatch(input);
+                if (res)
+                {
+                    Console.WriteLine("The Entered Name is Valid");
+                }
+                else
+                {
+                    Console.WriteLine("The Entered Name is Invalid");
+                }
+            }
+            public void Validating()
+            {
+                Regex regex = new Regex(pattern);
+            }
+        }
+    }
